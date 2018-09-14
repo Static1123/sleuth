70998fa6da5f472bffde9f2bc1d00fab63e8e46a
Liang.Yang5 <liang.yang5@geely.com>
2018-09-14 09:02:15 +0800

    sleuth test

--- zipkin-server/src/main/java/com/yl/zipkinserver/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/yl/zipkinserver/ZipkinServerApplication.java
@@ -3,8 +3,11 @@ package com.yl.zipkinserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
+
+import javax.sql.DataSource;
 
 /**
  * @author Static
@@ -12,8 +15,12 @@ import zipkin.server.EnableZipkinServer;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZipkinServer
-@EnableZipkinStreamServer
+//@EnableZipkinStreamServer
 public class ZipkinServerApplication {
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource dataSource) {
+        return MySQLStorage.builder().datasource(dataSource).executor(Runnable::run).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
